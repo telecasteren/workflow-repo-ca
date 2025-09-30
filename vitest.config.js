0c02542903a65a3e4725791e19f6821b0dc30b9a
telecasteren <127958662+telecasteren@users.noreply.github.com>
2025-09-30 20:46:13 +0200

    made sure vitest excludes all e2e tests when running

--- vitest.config.js
+++ vitest.config.js
@@ -4,6 +4,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    exclude: ["**/node_modules/**", "**/tests/e2e-tests/**"],
+    exclude: ["**/node_modules/**", "**/tests/e2e/**"],
   },
 });
